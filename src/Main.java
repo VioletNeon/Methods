a412fef9880ec3b02dba22594d5b004ec3fcfe0f
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-09 13:11:49 +0300

    Task-3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         checkLeapYear((short) 2024);
         defineAppVersionAndOS((byte) 1, (short) 2020);
+
+        short totalCountOfDeliveryDays = getTotalCountOfDeliveryDays((short) 95);
+
+        System.out.println("Итоговое количество дней доставки: " + totalCountOfDeliveryDays);
     }
 
     public static void checkLeapYear(short year) {
@@ -40,5 +44,24 @@ public class Main {
         } else if (isIOS) {
             System.out.println("Установите обычную версию приложения для iOS по ссылке");
         }
+    }
+
+    public static short getTotalCountOfDeliveryDays(short deliveryDistance) {
+        boolean isLessZeroKM = deliveryDistance < 20;
+        boolean isBetweenTwentyAndSixtyKM = deliveryDistance > 20 && deliveryDistance <= 60;
+        boolean isBetweenSixtyAndOneHundredKM = deliveryDistance > 60 && deliveryDistance <= 100;
+        boolean isMoreThanOneHundredKM = deliveryDistance > 100;
+
+        if (isLessZeroKM) {
+            return 1;
+        } else if (isBetweenTwentyAndSixtyKM) {
+            return 2;
+        } else if (isBetweenSixtyAndOneHundredKM) {
+            return 3;
+        } else if (isMoreThanOneHundredKM) {
+            throw new IllegalArgumentException("Доставки на расстояние: " + deliveryDistance + " км - нет");
+        }
+
+        return 0;
     }
 }
