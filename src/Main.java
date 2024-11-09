3be1536ff88d66dfe67410beb1ff46697766f8cd
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-09 12:56:02 +0300

    Task-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         checkLeapYear((short) 2024);
+        defineAppVersionAndOS((byte) 1, (short) 2020);
     }
 
     public static void checkLeapYear(short year) {
@@ -17,6 +20,25 @@ public class Main {
         } else {
             System.out.println(year + " год - невисокосный год");
         }
+    }
 
+    public static void defineAppVersionAndOS(byte clientOS, short clientDeviceYear) {
+        boolean isAndroid = clientOS == 1;
+        boolean isIOS = clientOS == 0;
+        short currentYear = (short) LocalDate.now().getYear();
+
+        boolean isBeforeConditionalYear = clientDeviceYear < currentYear;
+
+        if (isAndroid && isBeforeConditionalYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (isAndroid) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        }
+
+        if (isIOS && isBeforeConditionalYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (isIOS) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        }
     }
 }
