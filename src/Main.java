a5b9c3b18cf35814e5c61f6884067b91f9c2be86
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-09 12:33:22 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkLeapYear((short) 2024);
+    }
+
+    public static void checkLeapYear(short year) {
+        short startLeapYear = 1584;
+        short yearsDifference = (short) (year - startLeapYear);
+
+        boolean isForthYear = yearsDifference % 4 == 0;
+        boolean isHundredYear = yearsDifference % 100 == 0;
+        boolean isFourthHundredYear = yearsDifference % 400 == 0;
+        boolean isLeapYear = isFourthHundredYear || (!isHundredYear && isForthYear);
+
+        if (isLeapYear) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - невисокосный год");
+        }
+
     }
 }
